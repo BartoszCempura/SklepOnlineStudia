21da96b7d91e34ed94bd12344e79a3ffcfa3c400
cempubar <cempura.bartosz@gmail.com>
2024-12-10 22:51:51 +0100

    Creation of basics

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// definiujemy ścieżki do template
+$baseTemplate = __DIR__ . '/strona/templates/base.html'; 
+$page = $_GET['page'] ?? 'home'; // default
+$contentPath = __DIR__ . "/strona/templates/{$page}.html";
+
+// sprawdzamy czy base template jest
+if (file_exists($baseTemplate)) {
+    
+    $baseHtml = file_get_contents($baseTemplate);
+
+    if (file_exists($contentPath)) {
+        $contentHtml = file_get_contents($contentPath);
+    } else {
+        $contentHtml = '<h2>Page Not Found</h2>';
+    }
+
+    // podmieniamy blok {{ content }} na zawartość $page
+    $finalOutput = str_replace('{{ content }}', $contentHtml, $baseHtml);
+
+    // generujemy sumę
+    echo $finalOutput;
+} else {
+    echo "Error: Base template not found.";
+}
+?>
